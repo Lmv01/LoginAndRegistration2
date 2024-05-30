9af4f337801f49fc2ec9eadebc1179ce094c6837
per2 <leonardkwak1@gmail.com>
2024-05-30 09:49:14 -0700

    kkj

---
+++ app/src/main/java/com/mistershorr/loginandregistration/ReplyAdapter.kt
@@ -0,0 +1,29 @@
+package com.mistershorr.loginandregistration
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ReplyAdapter (var dataSet: MutableList<>) : RecyclerView.Adapter<ReplyAdapter.ViewHolder>() {
+
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        init {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReplyAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_sleep, parent, false)
+        val holder = ViewHolder(view)
+        return holder
+    }
+    override fun onBindViewHolder(holder: ReplyAdapter.ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+}
